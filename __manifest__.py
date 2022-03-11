1216a4cb54d2b143758d86674c1d3fd9723f1d91
Myke Rodas <mrodas@pitaya.tech>
2022-03-10 21:43:51 -0600

    commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Signature in Stock Picking",
-    'summary': """...""",
+    'summary': """Field for signature in delivery note""",
     'description': """
-        ..
+        Add signature field in Alborán depending on validation in Stock Picking type, which is printed in Stock Picking report
     """,
     'author': "Gt Alchemy Development",
     'license': 'LGPL-3',
     'support': 'developmentalchemygx@gmail.com',
-    'category': 'Extra Tools',
+    'category': 'Warehouse Management',
     'version': '15.0.1',
     'data': [
         'views/stock_picking_views.xml',

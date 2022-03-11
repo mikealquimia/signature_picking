7eae87f08a09a5b2891a4ab5503db7ef794bb0ab
Myke Rodas <mrodas@pitaya.tech>
2022-03-11 10:47:21 -0600

    commit version 13

--- signature_picking/__manifest__.py
+++ signature_picking/__manifest__.py
@@ -9,7 +9,7 @@
     'license': 'LGPL-3',
     'support': 'developmentalchemygx@gmail.com',
     'category': 'Warehouse Management',
-    'version': '15.0.1',
+    'version': '13.0.1',
     'data': [
         'views/stock_picking_views.xml',
         'reports/report_picking.xml',
